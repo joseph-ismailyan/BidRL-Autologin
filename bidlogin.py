dbaa61ef6aeb37c312d08cca67a3f34f3598ee3e
Joseph Ismailyan <miniman796@gmail.com>
2016-04-23 14:35:52 -0700

    Added files via upload

---
+++ bidlogin.py
@@ -0,0 +1,26 @@
+# prompts for password and performs a login 
+
+import os, sys, getpass
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
+user = {'name': 'YOUR EMAIL HERE', 'pw': ''} # hardcode your own email here
+user['pw'] = getpass.getpass('Enter Password: ', ) # no need to hardcode password
+browser = webdriver.Chrome()
+browser.get('https://www.bidrl.com/login')
+browser.set_window_position(0, 0)
+browser.maximize_window()
+userName = browser.find_element_by_id('user_name')
+password = browser.find_element_by_name('password')
+login = browser.find_element_by_name('login')
+actions = ActionChains(browser)
+actions.move_to_element(userName).send_keys(user['name'])
+actions.send_keys(Keys.TAB).send_keys(user['pw'])
+actions.move_to_element(login).click()
+actions.perform()
+
+
+
+
+
